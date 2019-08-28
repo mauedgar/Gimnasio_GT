991ac54103e6c66600bc3765c62f8fb3c2f1326f
Mauricio Mealla <mauricio.mealla@hotmail.com>
2019-08-28 02:57:13 -0300

    Agregar archivos de proyecto.

---
+++ Gimnasio_GT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gimnasio_GT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
